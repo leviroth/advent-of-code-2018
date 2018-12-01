9a73e67005150d10b039f34e6fcfffe31e3cfe5e
Levi Roth <levimroth@gmail.com>
2018-12-01 09:50:34 -0500

    Add day 01 part 02

--- src/day01.ml
+++ src/day01.ml
@@ -3,17 +3,40 @@ open! Import
 
 let date = 1
 
-module Part1 = struct
+module Input = struct
   type input = int list
-
-  let part = 1
-
   let parse file =
     Sexp.load_sexps_conv_exn file Int.t_of_sexp
+end
+
+module Part01 = struct
+  include Input
+  let part = 1
 
   let solve input =
     List.fold input ~init:0 ~f:(+)
 end
 
+module Part02 = struct
+  include Input
+  let part = 2
+
+  let solve input =
+    let input_cycle = Sequence.cycle_list_exn input in
+    Sequence.delayed_fold
+      input_cycle
+      ~init:(0, Int.Set.empty)
+      ~f:(fun (current_frequency, seen_frequencies) change ~k ->
+          match Set.mem seen_frequencies current_frequency with
+          | true -> current_frequency
+          | false ->
+            let next_frequency = current_frequency + change in
+            let next_seen = Set.add seen_frequencies current_frequency in
+            k (next_frequency, next_seen))
+      ~finish:fst
+end
+
 let parts : (module Solution) list =
-  [ (module Part1) ]
+  [ (module Part01)
+  ; (module Part02)
+  ]
